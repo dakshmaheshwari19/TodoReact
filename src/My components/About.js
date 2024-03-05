00e4fa9f3b099aff52fef77b8da3e9c380fca0a7
Daksh Maheshwari <dakshmaheshwari193@gmail.com>
2024-03-05 11:28:07 +0530

    fix:{minor fixes in the site content}

--- src/My components/About.js
+++ src/My components/About.js
@@ -6,7 +6,7 @@ export const About = () => {
     <h1 class="display-4">Hello, peeps!</h1>
     <p class="lead">This is a simple React application, a ToDo application to list your daily tasks.</p>
     <hr class="my-4"/>
-    <p>It uses angular framework by which you can access the website without reloading the page.This website is developed by Daksh Maheshwari. </p>
+    <p>It uses react framework by which you can access the website without reloading the page.This website is developed by Daksh Maheshwari. </p>
     <a class="btn" style={{backgroundColor:"black",color:"#D7D9CE"}} href="https://www.linkedin.com/in/daksh-maheshwari-157712238/" role="button">contact us</a>
   </div>
   )
--- src/My components/Footer.js
+++ src/My components/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => {
   return (
     <div className='bg-dark text-light py-3' >
       <p className='text-center'>
-      Copyright &copy; MyTodosList.com
+      Copyright &copy; Daksh Maheshwari
       </p>
       </div>
   )
